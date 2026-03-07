6764f1626ac0982a45aafa5677e7d455ef3bd8a1
LuisHBM111 <l.bobadillam@uniandes.edu.co>
2026-03-06 20:39:02 -0500

    First Commit

---
+++ app/src/main/java/firstapp/HomeActivity.kt
@@ -0,0 +1,18 @@
+package firstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.uniandes.interactivemapuniandes.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        // Adjust padding for system bars if necessary, 
+        // though CoordinatorLayout often handles this with fitsSystemWindows
+    }
+}

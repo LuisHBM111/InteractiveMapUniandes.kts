cf50dbd677df7bd928ac5f9b335bf12b57525cf3
LuisHBM111 <l.bobadillam@uniandes.edu.co>
2026-04-24 20:45:49 -0500

    - Room implemented

---
+++ app/src/main/java/interactivemapuniandes/model/data/AppDatabase.kt
@@ -0,0 +1,36 @@
+package interactivemapuniandes.model.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import interactivemapuniandes.model.entity.ScheduleClassEntity
+import interactivemapuniandes.model.entity.ScheduleEntity
+
+@Database(
+    entities = [
+        ScheduleEntity::class,
+        ScheduleClassEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun scheduleDao(): ScheduleDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "interactive_map_uniandes.db"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
+}
